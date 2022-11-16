08bd76e6a4b75eb158aa9482794be625d5465965
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2022-11-16 13:54:38 +0000

    add: header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,20 @@
 import type { AppProps } from 'next/app'
+import Image from 'next/image';
 
 import { GlobalStyles } from '../styles/global'
+
+import logoImg from "../../public/logo.svg"
+import { Container, Header } from '../styles/pages/app';
 
 GlobalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
