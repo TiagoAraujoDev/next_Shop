ddcfe4c107fab068e92734f1a3194989556d0aae
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2022-11-18 11:53:38 +0000

    feat: Stripe config and products data fetching

---
+++ src/lib/stripe.ts
@@ -0,0 +1,11 @@
+import Stripe from "stripe";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+export const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY! ,{
+  apiVersion: "2022-11-15",
+  appInfo: {
+    name:  "Nextjs_Shop",
+  },
+});
